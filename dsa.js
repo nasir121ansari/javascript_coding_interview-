4787b4b382010cc6a0af3c420d1cbc71620beda2
Md Nasir Ansari <nasir121ansari@gmail.com>
2024-08-30 16:16:18 +0530

    Update dsa.js

--- dsa.js
+++ dsa.js
@@ -704,6 +704,24 @@ function sumOfDiagonal (matrix,dimension) {
 console.log(sumOfDiagonal(matrix,dimension))
 
 
+// 32.
+// Custom map function implementation
+function customMap(array, callback) {
+  const result = [];
+  for (let i = 0; i < array.length; i++) {
+      result.push(callback(array[i], i, array));
+  }
+  return result;
+}
+
+// Usage example
+const numbers = [1, 2, 3, 4, 5];
+const doubled = customMap(numbers, (num) => num * 2);
+
+console.log(doubled); // Output: [2, 4, 6, 8, 10]
+
+
+
 
 
 
